a4b692e3b03abc01c0bd4f203b40cb1928109e03
LourdesGarcia <lourdesgarciafyl@gmail.com>
2023-08-05 12:31:03 -0300

    agrego el navbar, el card group y el input

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import './App.css'
 import 'bootstrap/dist/css/bootstrap.min.css';
+import TaskHead from './components/TaskHead';
 
 function App() {
 
   return (
     <>
+    <TaskHead></TaskHead>
     </>
   )
 }
---
+++ src/components/TaskHead.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import NavBar from "./NavBar.jsx"
+import Task from './task/Task.jsx';
+
+const TaskHead = () => {
+    return(
+        <>
+         <NavBar></NavBar>
+        <Task></Task>
+        </>
+    )
+}
+export default TaskHead;
